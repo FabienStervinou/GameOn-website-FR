d065771a4f37ff86ab467fb6d77b7356c710b9f2
FabienStervinou <stervinoufabien@gmail.com>
2021-06-22 12:14:10 +0200

    Add for attribute in form & css animation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,24 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClose = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
+const content = document.querySelectorAll(".content")[0];
 
-// launch modal event
+// launch and close modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
+  content.style.animationName = "modalopen";
   modalbg.style.display = "block";
 }
 
-
+function closeModal() {
+  content.style.animationName = "modalclose";
+  content.style.animationFillMode = "forwards";
+  setTimeout(() => {
+    modalbg.style.display = "none";
+  }, 800);
+}
